d121f5882fcc541308bf0d28c7ccdfdec35894b0
huangyuxian442-lang <huangyuxian442@gmail.com>
2026-02-28 00:14:15 +0630

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("🎬 Hollywood Movie Analysis")
+st.write("Welcome to my first app!")
+
+# User input စမ်းကြည့်မယ်
+movie = st.text_input("What is your favorite movie?")
+if movie:
+    st.write(f"Oh! I love {movie} too!")
